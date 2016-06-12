79b08e2efa07e2c016ad3217337cdf3d36578ac8
keisei_1092 <keisei_1092@yahoo.co.jp>
2016-06-13 04:32:03 +0900

    さっくりコントローラー中身書く

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,24 +1,51 @@
 # coding: utf-8
 
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :update, :edit:, :destroy]
+
   def index
+    @articles = Article.all
   end
 
   def show
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.create(article_params)
+    if @article.save
+      redirect_to article_path
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
   def update
+    if @article.update(article_params)
+      redirect_to article_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @article.destroy
+    redirect_to article_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
