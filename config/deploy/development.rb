b2d23ebcdbafbf92d88d7e6986f19a225524074a
keisei_1092 <keisei_1092@yahoo.co.jp>
2016-06-13 06:50:09 +0900

    sshにしてみる

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -7,6 +7,7 @@ server '157.7.53.185',
 user: 'root',
 roles: %w{web app db},
 ssh_options: {
- auth_methods: %w(password),
- password: 'Q4Uodoh5dCcfapX1'
+  keys: %w(~/.ssh/conoha_default.key),
+  forward_agent: true,
+  auth_methods: %w(publickey)
 }
